72917da8055fc9fff4fb3598871e17be7175b233
fadrian06 <franyeradriansanchez@gmail.com>
2025-01-23 21:23:27 -0400

    simplify setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,5 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/etarn/',
   integrations: [svelte()],
-  vite: {
-    server: {
-      proxy: {
-        '/api': 'http://localhost/etarn/api'
-      }
-    }
-  }
 });
