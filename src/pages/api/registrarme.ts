c8d27751b23b3c67252e812fdc7359228f49ad82
fadrian06 <franyeradriansanchez@gmail.com>
2025-01-26 01:47:31 -0400

    chore(biome): add biome as a local dependency

--- src/pages/api/registrarme.ts
+++ src/pages/api/registrarme.ts
@@ -1,14 +1,15 @@
 import type { APIRoute } from 'astro'
-import db from '@/database/db'
 import { hash } from 'bcrypt'
+import { saveUser } from '@/services/user'
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData()
 
-  db.run('insert into users (id, email, password) values ($id, $email, $password)', {
-    $id: crypto.randomUUID(),
-    $email: formData.get('email'),
-    $password: await hash(formData.get('password')?.toString() || '', 10)
+  await saveUser({
+    id: crypto.randomUUID(),
+    email: formData.get('email') as string,
+    password: await hash(formData.get('password')?.toString() || '', 10),
+    admin: true
   })
 
   return redirect('/app')
